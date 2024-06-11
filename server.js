537b99a14bca899ee26ec4639ecf01cf0c802240
MathiusFarfan <mathius1998@hotmail.com>
2024-06-11 09:25:01 -0500

    comit18

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const { google } = require('googleapis');
 const path = require('path');
 const app = express();
 
+require('dotenv').config();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Configuración de OAuth2
 const oAuth2Client = new google.auth.OAuth2(
-  '305681698109-qe2crqmglgm92fh4vk86gqseg5qosdma.apps.googleusercontent.com',
-  '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',
-  'https://prueba-q44y.onrender.com/oauth2callback'
-  //'https://prueba-q44y.onrender.com/oauth2callback'
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URI
 );
 
 // Servir el archivo HTML
@@ -29,28 +29,6 @@ app.get('/auth', (req, res) => {
   res.redirect(authUrl);
 });
 
-//-------------------
-// Inicializar el cliente de Google Tag Manager
-const gtm = google.tagmanager({ version: 'v2', auth: oAuth2Client });
-
-async function getContainerName() {
-  try {
-    // Obtener la información del contenedor
-    const container = await gtm.accounts.containers.get({
-      path: 'accounts/6228833093/containers/183956141'
-    });
-
-    // Extraer el nombre del contenedor
-    const containerName = container.data.name;
-
-    console.log('Nombre del contenedor:', containerName);
-  } catch (error) {
-    console.error('Error al obtener el nombre del contenedor:', error);
-  }
-}
-// Llamar a la función para obtener el nombre del contenedor
-getContainerName();
-//---------------------
 // Ruta de redirección para el callback de OAuth
 app.get('/oauth2callback', async (req, res) => {
   const { code } = req.query;
@@ -64,31 +42,20 @@ app.post('/update-gtm-table', async (req, res) => {
   const { inputId, outputId } = req.body;
   try {
     const gtm = google.tagmanager({ version: 'v2', auth: oAuth2Client });
-    console.log('hola');
-    // Obtener la última versión del contenedor
-    const container = await gtm.accounts.containers.get({
-      path: 'accounts/6228833093/containers/183956141'
-      //path: 'accounts/6228833093/containers/183956141'
-      //accountId: '6228833093',
-      //containerId: '183956141'
-      
+    const containerVersion = await gtm.accounts.containers.versions.latest({
+      accountId: process.env.GTM_ACCOUNT_ID,
+      containerId: process.env.GTM_CONTAINER_ID,
     });
-    const latestVersion = container.data.containerVersionId;
-    console.log('hola2');
-    console.log(latestVersion);
-    // Actualizar la versión del contenedor con los nuevos valores
-    const updatedContainer = await gtm.accounts.containers.versions.update({
-      path: 'container/accounts/6228833093/containers/183956141/workspaces/5/variables',
-      requestBody: {
-        ...container.data,
-        variable: container.data.variable.map(v => {
-          if (v.name === 'varprueba') {
-            v.parameter.push({ key: 'input', value: inputId });
-            v.parameter.push({ key: 'output', value: outputId });
-          }
-          return v;
-        })
-      }
+
+    const variable = containerVersion.data.variable.find(v => v.name === process.env.GTM_VARIABLE_NAME);
+    variable.parameter.push({ key: 'input', value: inputId });
+    variable.parameter.push({ key: 'output', value: outputId });
+
+    await gtm.accounts.containers.versions.update({
+      accountId: process.env.GTM_ACCOUNT_ID,
+      containerId: process.env.GTM_CONTAINER_ID,
+      containerVersionId: containerVersion.data.containerVersionId,
+      resource: containerVersion.data,
     });
 
     res.status(200).json({ message: 'Table updated successfully' });
